109d666f94053abfcad0ad10a3e6627a0267a6b5
dan_fred <dannyfrederick360@gmail.com>
2018-12-04 23:33:53 -0500

    init

---
+++ public/js/init.js
@@ -0,0 +1,12 @@
+(function($){
+  $(function(){
+    $('.parallax').parallax();
+  });
+
+  $(function(){
+
+    $('.sidenav').sidenav();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
+
